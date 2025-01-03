b9c77501c4c69281e18b480dfb5e7dd5044f7dd9
digitally <digitallytechno@proton.me>
2025-01-03 21:34:42 +0100

    Add files via upload

---
+++ src/include/timeval.h
@@ -0,0 +1,60 @@
+#ifndef TIMEVAL_H
+#define TIMEVAL_H 1
+
+/***************************************************************************
+
+ YAM - Yet Another Mailer
+ Copyright (C) 1995-2000 Marcel Beck
+ Copyright (C) 2000-2019 YAM Open Source Team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ YAM Official Support Site : http://www.yam.ch
+ YAM OpenSource project     : http://sourceforge.net/projects/yamos/
+
+ $Id$
+
+***************************************************************************/
+
+// since the Amiga's timeval structure was renamed to
+// "struct TimeVal" in OS4 (to prevent clashes with the POSIX one)
+// we require to define that slightly compatible structure on our
+// own in case we compile YAM for something else than OS4 or in case
+// an older SDK is used.
+#if !defined(__amigaos4__) || !defined(__NEW_TIMEVAL_DEFINITION_USED__)
+#include <exec/io.h>
+
+
+struct TimeVal
+{
+  ULONG Seconds;
+  ULONG Microseconds;
+};
+
+struct TimeRequest
+{
+  struct IORequest Request;
+  struct TimeVal   Time;
+};
+
+#define TIMEVAL(x)  (APTR)(x)
+
+#else
+
+#define TIMEVAL(x)  (x)
+
+#endif
+
+#endif /* TIMEVAL_H */
